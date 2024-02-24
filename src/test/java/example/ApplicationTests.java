1264577dc3a33aecab0c5d06f49ef03735bd4af9
edward <edward.rosenberg@outlook.com>
2024-02-23 22:25:09 -0600

    update dependencies, maven repos and fix unit test

--- src/test/java/example/ApplicationTests.java
+++ src/test/java/example/ApplicationTests.java
@@ -2,10 +2,10 @@ package example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@ContextConfiguration
 @RunWith(SpringRunner.class)
 public class ApplicationTests {
 
